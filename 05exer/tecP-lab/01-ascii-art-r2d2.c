171dcb8c2da24be824c8ec9053e878f1674cac26
marcos-bah <marcos.barbosa@unifei.edu.br>
2020-05-19 21:00:42 -0300

    first week update

---
+++ 05exer/tecP-lab/01-ascii-art-r2d2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    printf("\n\n");
+    printf("          _____                                  \n");
+    printf("         / ()  \\                                 \n");
+    printf("      __|_______|__                                \n");
+    printf("     |  |_______|  |                               \n");
+    printf("     |__|  ===  |__|      https://github.com/       \n");
+    printf("      | |   O   | |           marcos-bah             \n");
+    printf("      | |__ O __| |                                  \n");
+    printf("     |~  \\_____/  ~|                               \n");
+    printf("    /=\\    /=\\    /=\\                            \n");
+    printf(" __[__]___[__]___[__]____                            \n");
+    printf("\n\n");
+
+    return 0;
+}
---
+++ 05exer/tecP-lab/02-encode-caracters.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("dec=%d encode=%c hexa=%x\n");
+    return 0;
+}
